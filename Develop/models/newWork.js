f3a8fe42cf4a4f33103c99450e6c7f2bae7e072d
Sara Wilson <sarakatewilson98@gmail.com>
2021-03-15 23:05:48 -0400

    adding model and newWork.js

--- Develop/models/newWork.js
+++ Develop/models/newWork.js
@@ -2,5 +2,42 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const fitnessSchema = new Schema({
-    
-})
+    day: {
+        type: Date,
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number
+
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+}, 
+{ timestamps: true });
+
+const Workout = mongoose.model('Workout', fitnessSchema);
+module.exports = Workout;
