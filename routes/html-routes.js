371838708b942027f386bdb73c581e3dffe85fe3
Sara Wilson <sarakatewilson98@gmail.com>
2021-04-01 15:32:47 -0500

    cleaning up html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,9 +3,6 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
 router.get('/exercise', function (req, res) {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
@@ -13,3 +10,4 @@ router.get('/stats', function (req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 module.exports = router
+
