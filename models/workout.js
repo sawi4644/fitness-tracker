056cc90205a5f3970aa275e626d282274039b7a0
Sara Wilson <sarakatewilson98@gmail.com>
2021-04-01 15:38:12 -0500

    updating workout.js working through updating fitnessSchema and adding a function to show the user the duration of their work out

--- models/workout.js
+++ models/workout.js
@@ -4,44 +4,55 @@ const Schema = mongoose.Schema;
 const fitnessSchema = new Schema({
     day: {
         type: Date,
-        default: Date.now
-    },
-    exercises: [
+        default: () => new Date()
+      },
+      exercises: [
         {
-            type: {
-                type: String,
-                trim: true,
-                required: true
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: [true, 'What was your workout?']
-            },
-            duration: {
-                type: Number,
-                required: [true, 'How long did you work out for?']
-            },
-            weight: {
-                type: Number
-
-            },
-            reps: {
-                type: Number
-            },
-            sets: {
-                type: Number
-            },
-            distance: {
-                type: Number
-            }
+          type: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise type"
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: "Enter an exercise name"
+          },
+          duration: {
+            type: Number,
+            required: "Enter an exercise duration in minutes"
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
         }
-    ]
-}, 
-{ timestamps: true });
+      ]
+    },
+    {
+      toJSON: {
+        // include any virtual properties when data is requested
+        virtuals: true
+      }
+    }
+  );
 
-
-
+  // adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration; //this will reduce to just show durations
+    }, 0);
+  });
 
 const Workout = mongoose.model('Workout', fitnessSchema);
+
 module.exports = Workout;
+
